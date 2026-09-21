2e277948ba27f86952d31c45ed120bc020e89188
Kyle Shaw <falconshaw@yahoo.com>
2026-09-21 08:34:06 -0500

    Updated main.py and cadet_data_manager.py to accept external file inputs. Also added 4th input for cadet_loss

--- src/cadet_data_manager.py
+++ src/cadet_data_manager.py
@@ -10,7 +10,7 @@ def read_csv_file(filepath):
         fieldnames = reader.fieldnames
         return list(reader), fieldnames
 
-def join_data(primary_list, secondary_list, primary_key, foreign_key):
+def join_different_data(primary_list, secondary_list, foreign_key):
     """
     Joins two datasets together based on matching key fields.
     Similar to a SQL JOIN.
@@ -25,6 +25,16 @@ def join_data(primary_list, secondary_list, primary_key, foreign_key):
             joined_data.append(merged_record)
     return joined_data
 
+def join_similar_data(first_list, second_list):
+    dict = {item['SSN']: item for item in second_list}
+    combined_list = []
+    for item in first_list:
+        match_id = item['SSN']
+        if match_id in dict:
+            merged_item = {**item, **dict[match_id]}
+            combined_list.append(merged_item)
+    return combined_list
+
 def filter_by_field(data_list, field_name, field_value):
     """Filters records where a specific field matches a given value."""
     filtered_list = [item for item in data_list if item[field_name] == field_value]
@@ -36,10 +46,11 @@ def find_record_by_id(data_list, id_field, id_value):
     return record
 
 
-def get_unique_values(data_list_1, data_list_2, data_list_3):
+def get_unique_values(data_list_1, data_list_2, data_list_3, data_list_4):
     """Gets all unique values for a specific field in the dataset."""
     two_lists = list(dict.fromkeys(data_list_1 + data_list_2))
-    all_lists = list(dict.fromkeys(two_lists + data_list_3))
+    three_lists = list(dict.fromkeys(two_lists + data_list_3))
+    all_lists = list(dict.fromkeys(three_lists + data_list_4))
     return all_lists
 
 
--- src/main.py
+++ src/main.py
@@ -6,25 +6,22 @@ This is a basic structure to get you started. Modify it for your project!
 """
 import io
 import csv
+import sys
 from typing import cast
+import argparse
 import cadet_data_manager as rf
 
 
-def load_files():
-    accessions_tracker, accession_header = rf.read_csv_file('data/accession_tracker_example.csv')
-    ussf_cadets_ssn, ussf_cadets_ssn_header = rf.read_csv_file('data/ussf_cadets_ssn_example.csv')
-    ussf_cadets, ussf_cadets_header = rf.read_csv_file('data/ussf_cadets_example.csv')
-    return accessions_tracker, accession_header, ussf_cadets_ssn, ussf_cadets_ssn_header, ussf_cadets, ussf_cadets_header
-
-
-def prepare_files(ussf_cadets_ssn, filtered_accessions_tracker, ussf_cadets):
-    accessions_data = rf.join_data(ussf_cadets_ssn, filtered_accessions_tracker, primary_key='Cadet_ID', foreign_key='SSN')
-    full_ussf_cadet_data = rf.join_data(accessions_data, ussf_cadets, primary_key='SSN', foreign_key='Cadet_ID')
+def prepare_files(ussf_cadets_ssn, filtered_accessions_tracker, ussf_cadets, cadet_loss):
+    active_cadet_data = rf.join_different_data(ussf_cadets_ssn, filtered_accessions_tracker, foreign_key='SSN')
+    loss_cadet_data = rf.join_different_data(cadet_loss, filtered_accessions_tracker, foreign_key= 'SSN')
+    accessions_data = rf.join_similar_data(active_cadet_data, loss_cadet_data)
+    full_ussf_cadet_data = rf.join_different_data(accessions_data, ussf_cadets, foreign_key='Cadet ID')
     return full_ussf_cadet_data
 
 
-def get_headers(accessions_tracker_header, ussf_cadet_ssn_header, ussf_cadets_header):
-    headers = rf.get_unique_values(accession_header, ussf_cadets_ssn_header, ussf_cadets_header)
+def get_headers(accession_header, ussf_cadet_ssn_header, ussf_cadets_header, cadet_loss):
+    headers = rf.get_unique_values(accession_header, ussf_cadet_ssn_header, ussf_cadets_header, cadet_loss)
     return headers
 
 def display_menu():
@@ -58,30 +55,30 @@ def handle_choice(choice, full_ussf_cadet_data, headers):
     if choice == "1":
         print("You chose option 1!")
         # TODO: Call your function here
-        requested_det = input("For which Detachment do you want to view cadet data: \n")
-        requested_det_data = rf.filter_by_field(full_ussf_cadet_data, 'Det', requested_det)
+        requested_det = input("For which Detachment do you want to view cadet data (001-940): \n")
+        requested_det_data = rf.filter_by_field(full_ussf_cadet_data, 'Detachment', requested_det)
         print(f"There are {len(requested_det_data)} USSF cadets from Det {requested_det} in the Accessions Tracker: \n")
         for item in requested_det_data:
-            print(f"Cadet ID: {item['Cadet_ID']}, Name: {item['FirstName']} {item['LastName']} \n")
+            print(f"Cadet ID: {item['Cadet ID']}, Name: {item['FirstName']} {item['LastName']} \n")
     elif choice == "2":
         print("You chose option 2!")
         # TODO: Call your function here
         cadet_id = input("Enter the cadet's Cadet_ID: \n")
-        cadet_records = rf.find_record_by_id(full_ussf_cadet_data, 'Cadet_ID', cadet_id)
+        cadet_records = rf.find_record_by_id(full_ussf_cadet_data, 'Cadet ID', cadet_id)
         cadet_data = next(iter(cadet_records), None)
         if cadet_data is None:
-            print(f"No cadet found with Cadet_ID {cadet_id}.")
+            print(f"No cadet found with Cadet ID {cadet_id}.")
         else:
             print(f"Cadet {cadet_data['FirstName']} {cadet_data['LastName']}'s email address is {cadet_data['email']}")
 
     elif choice == "3":
         print("You chose option 3!")
         # TODO: Call your function here
-        cadet_id = input("Enter the cadet's Cadet_ID: \n")
-        cadet_records = rf.find_record_by_id(full_ussf_cadet_data, 'Cadet_ID', cadet_id)
+        cadet_id = input("Enter the cadet's Cadet ID: \n")
+        cadet_records = rf.find_record_by_id(full_ussf_cadet_data, 'Cadet ID', cadet_id)
         cadet_data = next(iter(cadet_records), None)
         if cadet_data is None:
-            print(f"No cadet found with Cadet_ID {cadet_id}.")
+            print(f"No cadet found with Cadet ID {cadet_id}.")
         else:
             print(f"Cadet {cadet_data['FirstName']} {cadet_data['LastName']}'s phone number is {cadet_data['phone']}")
 
@@ -102,7 +99,7 @@ def handle_choice(choice, full_ussf_cadet_data, headers):
     return True
 
 
-def main(full_ussf_cadet_data):
+def main(full_ussf_cadet_data, headers):
     """
     Main application loop.
     Displays menu, gets user input, processes choices.
@@ -117,9 +114,14 @@ def main(full_ussf_cadet_data):
 
 
 if __name__ == "__main__":
-    accessions_tracker, accession_header, ussf_cadets_ssn, ussf_cadets_ssn_header, ussf_cadets, ussf_cadets_header = load_files()
+    parser = argparse.ArgumentParser(description="Process multiple files.")
+    parser.add_argument('-i', '--inputs', nargs= 4, required = True)
+    args = parser.parse_args()
+    accessions_tracker, accession_header = rf.read_csv_file(args.inputs[0])
+    ussf_cadets_ssn, ussf_cadets_ssn_header = rf.read_csv_file(args.inputs[1])
+    ussf_cadets, ussf_cadets_header = rf.read_csv_file(args.inputs[2])
+    cadet_loss, cadet_loss_header = rf.read_csv_file(args.inputs[3])
     filtered_accessions_tracker = rf.filter_by_field(accessions_tracker, 'Branch', 'USSF')
-    full_ussf_cadet_data = prepare_files(ussf_cadets_ssn, filtered_accessions_tracker, ussf_cadets)
-    headers = get_headers(accession_header, ussf_cadets_ssn_header, ussf_cadets_header)
-    main(full_ussf_cadet_data)
-
+    full_ussf_cadet_data = prepare_files(ussf_cadets_ssn, filtered_accessions_tracker, ussf_cadets, cadet_loss)
+    headers = get_headers(accession_header, ussf_cadets_ssn_header, ussf_cadets_header, cadet_loss_header)
+    main(full_ussf_cadet_data, headers)
